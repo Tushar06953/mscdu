42b5fc486272d606bf403a83d76299033ca51c1c
Tushar Gaur <102008005+Tushar06953@users.noreply.github.com>
2024-11-07 12:06:29 +0530

    Update long.cpp

--- long.cpp
+++ long.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int incSubseq(vector<int>& arr, int cur, int prev) {
+int longestIncreasingSubsequence(int arr[], int n) {
+    // Create a DP array to store the length of LIS ending at each index
+    vector<int> dp(n, 1);  // Initially, each element is a subsequence of length 1
 
-    if(cur == arr.size() ) return 0;
-    int notinc = incSubseq(arr, cur+1, prev);
-    int inc = 0;
-    if( (prev == -1) || (arr[cur] > arr[prev]) ) {
-        inc = 1 + incSubseq(arr, cur+1, cur);
-    }
-    
-    return max(inc, notinc);
-}
-
-int main()
-{
-    vector<int> arr = {10, 9, 2, 5, 3, 7, 101, 18, 1, 19};
-    // prev[i] stores best 
-    vector<int> prev(arr.size(), -1);
-    for(int i=arr.size()-1; i>0; i--){
-        for(int j=i-1; j>=0; j--){
-            if(arr[j] < arr[i]){
-                prev[i] = j;
-                break;
+    // Build the DP table
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
-    cout << incSubseq(arr, 0, -1);
+
+    // The length of the longest increasing subsequence will be the max value in dp
+    return *max_element(dp.begin(), dp.end());
+}
+
+int main() {
+    int arr[] = {10, 9, 2, 5, 3, 7, 101, 18};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "The length of the longest increasing subsequence is " << longestIncreasingSubsequence(arr, n) << endl;
+
     return 0;
 }
